0b86ea3281a9661ce676a2df71319e3efbef4467
lukeymac <lukey.mac@outlook.com>
2019-01-09 22:36:42 -0500

    Initial Commit

---
+++ Drifter/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Drifter
+//
+//  Created by Lucas Kisabeth on 1/9/19.
+//  Copyright © 2019 Lucas Kisabeth. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
